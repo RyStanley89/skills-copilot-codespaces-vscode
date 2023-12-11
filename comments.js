053ee648c9af2150d22d722eb62ad3f9a9c8e713
Ryan Stanley <rystanley89@github.com>
2023-12-11 17:02:00 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,37 @@
-const http = require('http');
+// Create web server
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, world!');
+// Import modules
+var http = require('http');
+var fs = require('fs');
+var qs = require('querystring');
+var ejs = require('ejs');
+var settings = require('./settings');
+var mysql = require('mysql');
+
+// Create server
+var server = http.createServer();
+var connection = mysql.createConnection({
+    host: settings.host,
+    user: settings.user,
+    password: settings.password,
+    database: settings.database
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
-});
+// Read template file
+var template = fs.readFileSync(__dirname + '/public_html/bbs.ejs', 'utf-8');
+
+// Read data from database
+function getComments(callback) {
+    connection.query('SELECT * FROM comments',
+    function (error, results) {
+        callback(results);
+    });
+}
+
+// Add data to database
+function addComment(data, callback) {
+    connection.query('INSERT INTO comments SET ?',
+    data, function (error, results) {
+        callback();
+    });
+}
